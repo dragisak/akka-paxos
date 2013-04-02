25dfb5066d3bfdd3253446f98fd285344bf22b7a
Dragisa Krsmanovic <dragisak@ticketfly.com>
2013-04-02 09:38:10 -0700

    Wait until all replicas reply

--- src/main/scala/com/dragisak/HelloPaxos.scala
+++ src/main/scala/com/dragisak/HelloPaxos.scala
@@ -14,7 +14,7 @@ object HelloPaxos extends App {
 
   val crashes = 20
   val cntLeaders = 10
-  val messages = 1000
+  val messages = 2000
 
   val acceptors = (for (i <- 0 until crashes * 2 + 1) yield (system.actorOf(Props[Acceptor], name = "acceptor-" + i))).toSet
   val replicas = (for (i <- 0 until crashes + 1) yield (system.actorOf(Props(new Replica(cntLeaders)), name = "replica-" + i))).toSet
@@ -26,18 +26,17 @@ object HelloPaxos extends App {
     replicas.foreach(_ ! req)
   }
 
-  Thread.sleep(5000)
+  Thread.sleep(10000)
 
   val res = Future.sequence(replicas.toSeq.map(r => (r ? GetState).mapTo[Seq[Operation]])).onSuccess {
     case s => {
      // s.foreach(o => println(o.map(_.id).mkString(",")))
       assert(s.toSet.size == 1, "All replicas must receive same sequence of events")
       assert(s.head.size == messages, "Expected %d messages but received %d".format (messages, s.head.size))
+      println("Bye.")
+      system.shutdown()
     }
   }
 
-  Thread.sleep(1000)
 
-  println("Bye.")
-  system.shutdown()
 }

9c3a519935c7c28d2366eaa48ae5edf1ed754c64
Dragisa Krsmanovic <dragisak@ticketfly.com>
2013-05-14 12:01:28 -0700

    Refactor test

--- src/test/scala/akkapaxos/AkkaPaxosSpec.scala
+++ src/test/scala/akkapaxos/AkkaPaxosSpec.scala
@@ -1,10 +1,7 @@
 package akkapaxos
 
 import akka.actor._
-import akka.pattern.ask
 import scala.concurrent.duration._
-import akka.util.Timeout
-import concurrent.{Await, Future}
 import akka.testkit.{ImplicitSender, TestKit}
 import org.scalatest.WordSpec
 import org.scalatest.matchers.MustMatchers
@@ -13,31 +10,37 @@ import org.scalatest.BeforeAndAfterAll
 class AkkaPaxosSpec(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
 with WordSpec with MustMatchers with BeforeAndAfterAll {
 
-  implicit val timeout = Timeout(5 seconds)
-  implicit val ec = system.dispatcher
-
   def this() = this(ActorSystem("AkkaPaxosSpec"))
 
+  val messages = 10000
 
   override def afterAll() {
     system.shutdown()
   }
 
+  case class Finished(state: Seq[Int])
+
   trait MyState extends DistributedState[Seq[Int], Int] {
     override def startState = Seq()
 
-    override def append(state: => Seq[Int], event: Int) = state :+ event
+    override def append(state: => Seq[Int], event: Int) = {
+      val newState = state :+ event
+      if (newState.size >= messages) self ! Finished(newState)
+      newState
+    }
   }
 
   "all replicas" must {
     "receive all messages in same order" in {
       val crashes = 6
-      val cntLeaders = 5
-      val messages = 10000
 
-      val acceptors = (for (i <- 0 until crashes * 2 + 1) yield (system.actorOf(Props[Acceptor[Int]], name = s"acceptor-$i"))).toSet
-      val replicas = (for (i <- 0 until crashes + 1) yield (system.actorOf(Props(new Replica[Seq[Int], Int](cntLeaders) with MyState), name = s"replica-$i"))).toSet
-      val leaders = for (i <- 0 until cntLeaders) yield (system.actorOf(Props(new Leader[Int](i, acceptors, replicas)), name = s"leader-$i"))
+      val numReplicas = crashes + 1
+      val numLeaders = 5
+      val numAcceptors = crashes * 2 + 1
+
+      val acceptors = (for (i <- 0 until numAcceptors) yield (system.actorOf(Props[Acceptor[Int]], name = s"acceptor-$i"))).toSet
+      val replicas = (for (i <- 0 until numReplicas) yield (system.actorOf(Props(new Replica[Seq[Int], Int](numLeaders) with MyState), name = s"replica-$i"))).toSet
+      val leaders = for (i <- 0 until numLeaders) yield (system.actorOf(Props(new Leader[Int](i, acceptors, replicas)), name = s"leader-$i"))
 
       Thread.sleep(100)
 
@@ -46,15 +49,13 @@ with WordSpec with MustMatchers with BeforeAndAfterAll {
         replicas.foreach(_ ! req)
       }
 
-      Thread.sleep(20000)
+      val res = receiveN(numReplicas, 60 seconds).asInstanceOf[Seq[Finished]]
 
-      val res = Await.result(Future.sequence(replicas.toSeq.map(r => (r ? GetState).mapTo[Seq[Int]])), 20.seconds)
+      res.size must be(numReplicas)
+      res.head.state.size must be(messages)
+      val s = res.map(_.state).toSet
 
-      res.size must be(replicas.size)
-      res.head.size must be (messages)
-      val s = res.toSet
-
-      s.size must be  (1)
+      s.size must be(1)
 
     }
   }
